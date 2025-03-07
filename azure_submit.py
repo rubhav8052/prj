42eb6766edf0ea13db0fbe0154aaa37983aa60cb
external-Shreyas-VeljibhaiBhanderi_pace <external.Shreyas.VeljibhaiBhanderi@in.bosch.com>
2025-03-07 08:51:23 +0000

    Updated workspace config for AML submit script

--- azure_submit.py
+++ azure_submit.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     source_directory = f'{os.path.dirname(os.path.abspath(__file__))}/{cfg["SRC"]}'
     input_ds_names = ['vdeepingestprod', 'mtl_workspace', 'cliff_image_dump_qa', 'cliff_image_dump_prod', 'cliff_image_dump_dev', 'cliff_request_dump_prod', 'cache']
 
-    ws = Workspace.from_config()
+    ws = Workspace(workspace_name='object_retrieval', subscription_id='c4f1c7f3-9206-409f-a333-5b89a516e5dd', resource_group='vdeep-ct-prod')
     ws.get_details()
 
     version = int(cfg["ENV"].split(':')[1]) if ':' in cfg["ENV"] else None
